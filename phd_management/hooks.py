a2188ca56d3b199d39003872da54c4dd532fe592
Pasan Wijerathne <e23442@eng.pdn.ac.lk>
2026-03-05 00:43:18 +0000

    Sprint 5: Exported Workflows and Custom Fields as fixtures

--- phd_management/hooks.py
+++ phd_management/hooks.py
@@ -253,4 +253,4 @@ require_type_annotated_api_methods = True
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
-fixtures = ["Workflow", "Workflow State", "Workflow Action", "Custom Field", "Role Permission for Page and Report"]
+fixtures = ["Workflow", "Workflow State", "Workflow Action", "Custom Field"]
